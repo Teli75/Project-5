031f20ae058ddb8ad996de76f3a2f219d51a0d6a
teli <7telita@gmail.com>
2023-10-13 15:19:38 -0700

    Iterate through 12 objects

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,27 @@
 //City or location
 //Refer to the mockups and the comments in the index.html file for an example of what info should be displayed on the page and
 // how it should be styled.
+const outerCardDiv = document.querySelector(".card");
 
 async function getUsers(url) {
- //try{
-    //const response = await fetch("https://randomuser.me/api/")
-    fetch(url)
+  //try{
+  //const response = await fetch("https://randomuser.me/api/")
+  fetch(url)
     .then(response => response.json())
-    .then(response => console.log(response.results));
+    //.then(data = data.result)
+    // .forEach(result => {
+    //     console.log(result);
+    // })
+    .then(data => {
+      data.results.forEach((result) => {
+        console.log(result);
+      });
+    });
+}
+
+// .then(data => {
+//     data.results.forEach(generateImage(data))
+// });
 
 //     if (!response.ok) throw new Error("Something went wrong");
 
@@ -22,6 +36,22 @@ async function getUsers(url) {
 //   } catch {
 
 //}
-}
 
-getUsers('https://randomuser.me/api/?results=12');
+// function generateCard(data) {
+//   const html = `<div class="card-img-container">
+//      <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+//                     </div>
+//                     <div class="card-info-container">
+//                         <h3 id="name" class="card-name cap">first last</h3>
+//                         <p class="card-text">email</p>
+//                         <p class="card-text cap">city, state</p>
+//                     </div>
+//                 </div> */
+
+// getUsers("https://randomuser.me/api/?results=12");
+
+// <img src='${data} alt>
+// <p> First User </p>
+// `;
+// outerCardDiv.insertAdjacentHTML("beforeend", html);
+// }
