3613f8ebe48d778b117f767b790fcc91c48ae80c
teli <7telita@gmail.com>
2023-10-13 16:21:34 -0700

    display the 12 users

--- js/scripts.js
+++ js/scripts.js
@@ -7,51 +7,34 @@
 //City or location
 //Refer to the mockups and the comments in the index.html file for an example of what info should be displayed on the page and
 // how it should be styled.
-const outerCardDiv = document.querySelector(".card");
+const outerCardDiv = document.querySelector(".gallery");
+//let employees;
 
 async function getUsers(url) {
-  //try{
-  //const response = await fetch("https://randomuser.me/api/")
+  try{
   fetch(url)
     .then(response => response.json())
-    //.then(data = data.result)
-    // .forEach(result => {
-    //     console.log(result);
-    // })
-    .then(data => {
-      data.results.forEach((result) => {
-        console.log(result);
-      });
-    });
+    .then(data => data.results.forEach(generateCard));
+} catch (error){
+    console.log(error);
+}
 }
 
-// .then(data => {
-//     data.results.forEach(generateImage(data))
-// });
 
-//     if (!response.ok) throw new Error("Something went wrong");
+function generateCard(data) {
+    console.log(data);
+ const html = `
+ <div class="card">
+ <div class="card-img-container">
+  <img class="card-img" src=${data.picture.large} alt="profile picture">
+ </div>
+ <div class="card-info-container">
+ <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+  <p class="card-text">${data.email}</p>
+ <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+ </div>
+ </div>`;
+outerCardDiv.insertAdjacentHTML("beforeend", html);
+}
 
-//     const data = await response.json();
-//     console.log(data);
-//   } catch {
-
-//}
-
-// function generateCard(data) {
-//   const html = `<div class="card-img-container">
-//      <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-//                     </div>
-//                     <div class="card-info-container">
-//                         <h3 id="name" class="card-name cap">first last</h3>
-//                         <p class="card-text">email</p>
-//                         <p class="card-text cap">city, state</p>
-//                     </div>
-//                 </div> */
-
-// getUsers("https://randomuser.me/api/?results=12");
-
-// <img src='${data} alt>
-// <p> First User </p>
-// `;
-// outerCardDiv.insertAdjacentHTML("beforeend", html);
-// }
+getUsers("https://randomuser.me/api/?results=12");
